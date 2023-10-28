1b136c403774a9ff0b97d9720c9d69065ac68307
= <=>
2023-10-28 14:35:48 +0000

    fix(client): Change dev environment to use dev domain

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    serviceUrl: 'http://trylinks.net', //todo: this must be changed to dev.trylinks.net when it is setup.
+    serviceUrl: 'http://dev.trylinks.net', 
   };
