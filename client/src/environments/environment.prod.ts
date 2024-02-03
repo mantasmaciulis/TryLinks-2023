47ab182c4b340712cc238750bb0b7752afd9a417
Mantas Maciulis <maciulispersonal@gmail.com>
2024-02-03 12:45:55 +0000

    feat(client): setup auth0 env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: false,
   serviceUrl: 'http://trylinks.net',
+  auth: {
+    domain: '${AUTH_DOMAIN}',
+    clientId: '${AUTH_CLIENT_ID}',
+    redirectUri: '${AUTH_REDIRECT_URI}',
+  },
   envName: ""
 };
