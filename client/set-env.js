490a7c36a0b0732b593b5f21e3039ed06c2b3342
Mantas Maciulis <maciulispersonal@gmail.com>
2024-02-03 15:12:36 +0000

    fix(cicd): set env for auth0 Previouly I forgot to set the required auth0 variables on the develop environment and only did for production.

--- client/set-env.js
+++ client/set-env.js
@@ -10,3 +10,12 @@ envContent = envContent.replace(/\$\{AUTH_CLIENT_ID\}/g, process.env.AUTH_CLIENT
 envContent = envContent.replace(/\$\{AUTH_REDIRECT_URI\}/g, process.env.AUTH_REDIRECT_URI);
 
 fs.writeFileSync(prodEnvPath, envContent, 'utf8');
+
+const devEnvPath = path.join(__dirname, 'src', 'environments', 'environment.dev.ts');
+let envDevContent = fs.readFileSync(devEnvPath, 'utf8');
+
+envDevContent = envDevContent.replace(/\$\{AUTH_DOMAIN\}/g, process.env.AUTH_DOMAIN);
+envDevContent = envDevContent.replace(/\$\{AUTH_CLIENT_ID\}/g, process.env.AUTH_CLIENT_ID);
+envDevContent = envDevContent.replace(/\$\{AUTH_REDIRECT_URI\}/g, process.env.AUTH_REDIRECT_URI);
+
+fs.writeFileSync(devEnvPath, envDevContent, 'utf8');
--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -1,5 +1,10 @@
 export const environment = {
     production: false,
     serviceUrl: 'http://dev.trylinks.net',
-    envName: "Dev"
+    envName: "Dev",
+    auth: {
+      domain: '${AUTH_DOMAIN}',
+      clientId: '${AUTH_CLIENT_ID}',
+      redirectUri: '${AUTH_REDIRECT_URI}',
+    },
   };
