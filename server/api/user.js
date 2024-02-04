a20c070b0a66e611c7b61b707d7c15e6f3831f38
Mantas Maciulis <maciulispersonal@gmail.com>
2024-02-04 12:45:14 +0000

    feat(server): remove user auth

--- server/api/user.js
+++ server/api/user.js
@@ -1,101 +1,47 @@
-var userDB = require('../db/user-queries')
-var bcrypt = require('bcryptjs')
+function login(req, res) {
+  const userID = req.auth.sub;
 
-function signUp (req, res, next) {
-  const username = req.body.username
-  const email = req.body.email
-  const password = req.body.password
-
-  // Check if username is used.
-  userDB.getUserByUsername(username)
-    .then(() => {
-      res.status(409)
-        .json({
-          status: 'error',
-          message: 'Username used'
-        })
+  userDB.getUserByUsername(userID)
+    .then((user) => {
+      req.session.user = user;
+      res.status(200).json({
+        status: 'success',
+        message: 'Login successful',
+        data: {
+          username: user.username,
+          last_tutorial: user.last_tutorial,
+          is_admin: user.is_admin
+        }
+      });
     })
     .catch(() => {
-      // Hashing
-      const salt = bcrypt.genSaltSync()
-      const hash = bcrypt.hashSync(password, salt)
-
-      userDB.createUser(username, email, hash)
+      userDB.createUser(userID)
         .then(() => {
-          res.status(200)
-            .json({
-              status: 'success',
-              message: `User ${username} registered`
-            })
+          res.status(200).json({
+            status: 'success',
+            message: `User registered`
+          });
         })
         .catch(err => {
-          res.status(500)
-            .json(err)
-        })
-    })
-}
-
-function login (req, res, next) {
-  const username = req.body.username
-  const password = req.body.password
-  console.log(username)
-  console.log(password)
-  console.log("hmm")
-
-  // Check if user exists.
-  userDB.getUserByUsername(username)
-    .then((user) => {
-      try {
-        if (bcrypt.compareSync(password, user.password)) {
-          req.session.user = user
-          res.status(200)
-            .json({
-              status: 'success',
-              message: 'Login successful',
-              data: {
-                username: user.username,
-                last_tutorial: user.last_tutorial,
-                is_admin: user.is_admin
-              }
-            })
-        } else {
-          res.status(401)
-            .json({
-              status: 'error',
-              message: 'Incorrect login or password'
-            })
-        }
-      } catch (err) {
-        // Handle error from bcrypt.compareSync
-        console.error(err);
-        res.status(500)
-          .json({
+          // Handle potential errors from createUser
+          console.error(err);
+          res.status(500).json({
             status: 'error',
-            message: 'Internal server error'
-          })
-      }
-    }).catch(() => {
-      res.status(404)
-        .json({
-          status: 'error',
-          message: 'Username not found'
-        })
-    })
+            message: 'Error creating user'
+          });
+        });
+    });
 }
 
 function update (req, res, next) {
 
-  const username = req.session.user.username
+  const userID = req.auth.sub;
   // Check if user exists.
-  userDB.getUserByUsername(username)
+  userDB.getUserByUsername(userID)
     .then((user) => {
-      const email = req.body.email
-      const password = req.body.password
       const lastTutorial = req.body.last_tutorial
-      const salt = bcrypt.genSaltSync()
-      const hash = password === null ? null : bcrypt.hashSync(password, salt)
-      userDB.updateUser(username, { email: email, password: hash, last_tutorial: lastTutorial })
-        .then(() => userDB.getUserByUsername(username))
+      userDB.updateUser(userID, {last_tutorial: lastTutorial })
+        .then(() => userDB.getUserByUsername(userID))
         .then((user) => {
           req.session.user = user
           res.status(200)
@@ -134,7 +80,6 @@ function logout (req, res, next) {
 }
 
 module.exports = {
-  signUp: signUp,
   login: login,
   update: update,
   logout: logout
