fdfd6b1a4fdc59057fb4ad38456c273645c6b229
Mantas Maciulis <maciulispersonal@gmail.com>
2024-04-02 18:00:25 +0100

    fix: print string

--- server/db/db-connect.js
+++ server/db/db-connect.js
@@ -6,6 +6,7 @@ var options = {
 
 var pgp = require('pg-promise')(options)
 var connectionString = process.env.DB_CONNECTION_STRING
+console.log("Database Connection String:", connectionString);
 var db = pgp(connectionString)
 
 module.exports = db
